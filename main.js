2b0973977611842f39ccfaa4cf60917cff432aa2
Renan Rohers Salvador <111884210+r0HERS@users.noreply.github.com>
2024-04-29 17:43:34 -0300

    tests playwright

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-// main.js
-
-// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost', 
+  user: 'root',
+  password: 'root',
+  database: 'universidade',
+  port: 3333
+});
+
+
+
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -14,27 +22,24 @@ const createWindow = () => {
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  connection.connect((err) => {
+    if (err) {
+      console.error('Erro ao conectar ao banco de dados:', err);
+      return;
+    }
+    console.log('Conexão bem-sucedida ao banco de dados MySQL');
+  });
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+
+  mainWindow.loadFile('index.html')
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Algumas APIs podem ser usadas somente depois que este evento ocorre.
 app.whenReady().then(() => {
   createWindow()
 
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. Você também pode colocar eles em arquivos separados e requeridos-as aqui.
---
+++ test/tests.spec.js
@@ -0,0 +1,44 @@
+const { test, expect, _electron: electron } = require('@playwright/test')
+
+test('example test', async () => {
+  const electronApp = await electron.launch({ args: ['main.js'] })
+  const isPackaged = await electronApp.evaluate(async ({ app }) => {
+    // This runs in Electron's main process, parameter here is always
+    // the result of the require('electron') in the main app script.
+    return app.isPackaged
+  })
+
+  expect(isPackaged).toBe(false)
+
+  // Wait for the first BrowserWindow to open
+  // and return its Page object
+  const window = await electronApp.firstWindow()
+  await window.screenshot({ path: 'intro.png' })
+
+  // close app
+  await electronApp.close()
+})
+
+
+test('Consulta de CEP válido retorna informações esperadas', async ({ page }) => {
+  // CEP válido para teste
+  const cep = '01001-000';
+
+  // Acessando a API para consultar o CEP
+  await page.goto(`https://viacep.com.br/ws/${cep}/json/`);
+
+  // Capturando o conteúdo da resposta da API
+  const responseText = await page.textContent('pre');
+
+  // Convertendo o conteúdo da resposta para um objeto JSON
+  const responseData = JSON.parse(responseText);
+
+  // Verificando se o CEP retornado é igual ao CEP consultado
+  expect(responseData.cep).toBe(cep);
+
+  // Verificando se os campos de endereço estão presentes na resposta
+  expect(responseData.logradouro).not.toBe(undefined);
+  expect(responseData.bairro).not.toBe(undefined);
+  expect(responseData.localidade).not.toBe(undefined);
+  expect(responseData.uf).not.toBe(undefined);
+});
