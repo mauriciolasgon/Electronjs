644e6291a006009ebffc372c697e27025d1b072c
mauriciolasgon <108753106+mauriciolasgon@users.noreply.github.com>
2024-04-29 10:50:55 -0300

    db-1

--- test/specs/test.e2e.ts
+++
@@ -1,9 +0,0 @@
-import { browser, $,expect} from '@wdio/globals'
-
-describe('Electron Testing', () => {
-    it('should print application title', async () => {
-        await expect($('h1')).toHaveText('Hello World!')
-        await browser.pause(5000)
-    })
-})
-
